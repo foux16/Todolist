946965e3e68ce61d8c344a3e233ad9018178c4ee
Felipe Alexandre Silva Lima <33532803+foux16@users.noreply.github.com>
2019-01-30 14:56:21 -0300

    Project Upload Complete

---
+++ todoeylist/todoeylist/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  todoeylist
+//
+//  Created by Felipe Silva Lima on 1/30/19.
+//  Copyright © 2019 Felipe Silva Lima. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title : String = ""
+    var done : Bool = false
+    
+}
---
+++ todoeylist/todoeylist/TodoeyListViewController.swift
@@ -0,0 +1,88 @@
+//
+//  ViewController.swift
+//  todoeylist
+//
+//  Created by Felipe Silva Lima on 1/28/19.
+//  Copyright © 2019 Felipe Silva Lima. All rights reserved.
+//
+
+import UIKit
+
+class TodoeyListViewController: UITableViewController {
+
+    //Criacao de variaveis
+    var itemArray = [Item]()
+    let userDefault = UserDefaults.standard
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+//        if let items = userDefault.array(forKey: "TodoListArray") as? [String] {
+//            itemArray = items
+//        }
+        let newItem = Item()
+        newItem.title = "Find Mike"
+        itemArray.append(newItem)
+        
+        
+    }
+
+    //MARK - Table View Datasource Methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row].title
+        
+        return cell
+        
+    }
+    
+    //MARK - Table View Delegate Methods
+    //Metodo que verifica qual celula foi selecionada.
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //Utilizando uma caracteristica da table view chamada acessory para marcar/desmarcar as tarefas selecionadas.
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    //MARK - Add New Itemms
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Today Item", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            //what will happen when the user toches de Add new Item
+            let newItem = Item()
+            
+            newItem.title = textField.text!
+            
+            self.itemArray.append(newItem)
+            
+            self.userDefault.set(self.itemArray as NSArray, forKey: "TodoListArray")
+            self.tableView.reloadData()
+            
+        }
+        alert.addAction(action)
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+        }
+        present(alert, animated: true, completion: nil)
+        
+    }
+}
+
+
+
